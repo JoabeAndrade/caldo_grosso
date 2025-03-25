36062e32e6d65c59fd889ad82b0cc9a15971d6ff
Joabe Andrade do Nascimento <joabeandrade2k01@gmail.com>
2025-03-25 16:31:25 -0300

    feat: Tela Home criada e componente info finalizado

--- app/index.tsx
+++ app/index.tsx
@@ -1,34 +1,143 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export default function Page() {
+export default function Screen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Image
+          source={require("../assets/images/banner1280x426_v1672623700.png")}
+          style={styles.image_banner}
+        />
       </View>
-    </View>
+      <View style={styles.info}>
+        <View style={styles.info_about}>
+          <Image
+            source={require("../assets/images/logo_v1672623700.png")}
+            style={styles.info_logo}
+          />
+          <View>
+            <TouchableOpacity>
+              <View style={styles.info_about_button}>
+                <Text style={styles.info_button_about1}>
+                  Caldo Grosso Restaurante
+                </Text>
+                <AntDesign name="right" size={20} color="black" />
+              </View>
+              <View>
+                <View style={styles.info_button_about2}>
+                  <MaterialCommunityIcons
+                    name="silverware-fork-knife"
+                    size={15}
+                    color="#7f929f"
+                  />
+                  <Text style={{ color: "#7f929f" }}>Bar & Restaurante</Text>
+                </View>
+                <View style={styles.info_button_about3}>
+                  <AntDesign name="clockcircleo" size={18} color="red" />
+                  <Text style={{ color: "red" }}>Fechada</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View>
+          <TouchableOpacity style={styles.info_description_button}>
+            <FontAwesome6 name="motorcycle" size={16} color="#7f8f9f" />
+            <Text style={{ color: "#7f8f9f", textDecorationLine: "underline" }}>
+              Tempo estimado 40 minutos
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.info_description_text}>
+            <FontAwesome name="shopping-basket" size={16} color="#7f8f9f" />
+            <Text style={{ color: "#7f8f9f", textDecorationLine: "underline" }}>
+              Pedido mínimo: R$ 20,00
+            </Text>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    padding: 24,
+    backgroundColor: "#f3f5f7",
   },
-  main: {
-    flex: 1,
+  image_banner: {
+    width: "100%",
+    height: 100,
+    resizeMode: "cover",
+  },
+  info: {
+    width: "100%",
+    height: 200,
+    backgroundColor: "#fff",
+    padding: 25,
+  },
+  info_about: {
+    flexDirection: "row",
     justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
+    alignItems: "center",
+    gap: 20,
   },
-  title: {
-    fontSize: 64,
+  info_logo: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+  },
+  info_about_button: {
+    flexDirection: "row",
+    gap: 15,
+  },
+  info_button_about1: {
+    fontSize: 15,
     fontWeight: "bold",
   },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+  info_button_about2: {
+    width: "70%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 5,
+    justifyContent: "center",
+    gap: 5,
+    backgroundColor: "#f8f9fa",
+    borderRadius: 25,
+  },
+  info_button_about3: {
+    marginTop: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+    borderWidth: 1,
+    borderColor: "red",
+    borderRadius: 25,
+    width: "50%",
+    padding: 5,
+  },
+  info_description_button: {
+    marginTop: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+  },
+  info_description_text: {
+    marginTop: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
   },
 });
